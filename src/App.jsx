183586063e0f0e2861b3ee083377310fc9e03516
Mukul Singhal <work.mukulsinghal@gmail.com>
2025-05-09 22:57:16 +0530

    handling all the files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import  Card from  './components/card.jsx'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <Card name='mukul'>
+        <h1> Hello World</h1>
+        <p> My name is Mukul Singhal</p>
+        <p> I am a software Engineer</p>
+      </Card>
+
+    </div>
+  )
+}
+
+export default App
